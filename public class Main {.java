41f138827389184c97e05e193f524a98a7b1120a
kr8tomadvocate <149992994+kr8tomadvocate@users.noreply.github.com>
2023-11-23 08:56:14 -0600

    woo

---
+++ public class Main {.java
@@ -0,0 +1,21 @@
+public class Main {
+    
+}
+www.biak-biak.com
+/page?key1const query = wixLocation.query;
+=value1&key2=value2 const query = wixLocation.query;
+=value1&key2=value2{ key1: "value1", key2: "value2" }
+const query = wixLocation.query;
+
+{
+    "kind": string,
+    "name": string,
+    "description": string,
+    "etag": string,
+    "blockInheritance": boolean,
+    "orgUnitId": string,
+    "orgUnitPath": string,
+    "parentOrgUnitId": string,
+    "parentOrgUnitPath": string
+  }
+  
